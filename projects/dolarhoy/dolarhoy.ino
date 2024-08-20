bfcf6f8495cec6ac9204044b1861f03dbee2d9c6
Arian Giles García <arian2822@gmail.com>
2024-08-19 23:06:20 -0300

    Changes

--- projects/dolarhoy/dolarhoy.ino
+++ projects/dolarhoy/dolarhoy.ino
@@ -6,9 +6,9 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "Davitel_49941_2.4";
-const char* password = "20362011877";
-const char* apiUrl = "https://dolarapi.com/v1/dolares/blue";
+const char *ssid = "Davitel_49941_2.4";
+const char *password = "20362011877";
+const char *apiUrl = "https://dolarapi.com/v1/dolares/blue";
 
 Button2 btn1(BUTTON_1);
 
@@ -19,14 +19,26 @@ int cursor_y = 60;
 Rect_t displayArea = {
     .x = 10,
     .y = 20,
-    .width = EPD_WIDTH - 20,
-    .height =  EPD_HEIGHT / 2 + 80
+    .width = EPD_WIDTH / 2,
+    .height = EPD_HEIGHT / 2
 };
 
-void displayDolarInfo()
-{
-    cursor_x = 20;
-    cursor_y = 60;
+Rect_t screen = {
+    .x = 0,
+    .y = 0,
+    .width = EPD_WIDTH,
+    .height = EPD_HEIGHT
+};
+
+// Struct to hold the parsed data
+struct DolarInfo {
+    int compra;
+    int venta;
+};
+
+// Function to get and parse the Dolar information from the API
+DolarInfo getDolarInfo() {
+    DolarInfo info = {0, 0}; // Default values in case of failure
 
     if (WiFi.status() == WL_CONNECTED) {
         HTTPClient http;
@@ -41,13 +53,8 @@ void displayDolarInfo()
             DeserializationError error = deserializeJson(doc, payload);
 
             if (!error) {
-                int compra = doc["compra"];
-                int venta = doc["venta"];
-
-                epd_clear_area(displayArea);
-
-                String info = "Compra: " + String(compra) + "\nVenta: " + String(venta);
-                write_string((GFXfont *)&FiraSans, info.c_str(), &cursor_x, &cursor_y, NULL);
+                info.compra = doc["compra"];
+                info.venta = doc["venta"];
             } else {
                 Serial.println("JSON Parsing failed");
             }
@@ -61,16 +68,40 @@ void displayDolarInfo()
         Serial.println("WiFi not connected");
     }
 
+    return info;
+}
+
+
+void displayDolarInfo(const DolarInfo &info) {
+    cursor_x = 20;
+    cursor_y = 60;
+
+    epd_clear_area(displayArea);
+
+    String line_compra = "Compra: $" + String(info.compra);
+    String line_venta = "Venta: $" + String(info.venta);
+
+    cursor_x = 20 + 20;
+    cursor_y = 60 + 60;
+
+    write_string((GFXfont *)&FiraSans, line_compra.c_str(), &cursor_x, &cursor_y, NULL);
+
+    cursor_x = 20 + 20 + 20;
+    cursor_y = 60 + 60 + 60;
+
+    write_string((GFXfont *)&FiraSans, line_venta.c_str(), &cursor_x, &cursor_y, NULL);
+
     epd_poweroff();
 }
 
-void buttonPressed(Button2 &b)
-{
-    displayDolarInfo();
+
+void buttonPressed(Button2 &b){
+    DolarInfo info = getDolarInfo();
+    displayDolarInfo(info);
 }
 
-void setup()
-{
+
+void setup() {
     Serial.begin(115200);
     epd_init();
 
@@ -84,12 +115,12 @@ void setup()
     btn1.setPressedHandler(buttonPressed);
 
     epd_poweron();
-    epd_clear();
-    displayDolarInfo(); // Display information on startup
+    epd_clear_area(screen);
+    DolarInfo info = getDolarInfo();
+    displayDolarInfo(info);
 }
 
-void loop()
-{
+void loop() {
     btn1.loop();
     delay(2);
 }
